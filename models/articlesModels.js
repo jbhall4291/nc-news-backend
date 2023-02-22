add30b1359ea71b520ad0e3ccaea86b1346b10bf
johnny <jbhall4291@gmail.com>
2023-02-22 09:46:30 +0000

    remove comments & console logs as per feedback

--- models/articlesModels.js
+++ models/articlesModels.js
@@ -11,16 +11,16 @@ exports.selectAllArticles = () => {
 };
 
 exports.selectArticleById = (article_id) => {
-  // if the article_id is NOT a number then reject promise
+  
   if (isNaN(article_id) === true) {
-    console.log("article id is not a number!!!!!!");
+    
     return Promise.reject("article_id is not a number");
   }
 
   const selectArticleByIdQueryString = `SELECT * FROM articles WHERE article_id = $1;`;
 
   return db.query(selectArticleByIdQueryString, [article_id]).then((result) => {
-    //if rowCount is 0 (i.e. no results for that article id) then reject promise
+    
     if (result.rowCount === 0) {
       return Promise.reject("article_id not found");
     } else {
